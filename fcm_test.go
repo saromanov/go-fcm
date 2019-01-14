38e64582bcaa6a14e7e212a8477f65b16f1aa852
Sergey <xxsmotur@gmail.com>
2019-01-14 11:46:36 +0500

    Mocing of fcm server for tests

---
+++ fcm_test.go
@@ -0,0 +1,30 @@
+package fcm
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	a = New("testKey")
+	assert.NotNil(t, a, "unable to create object")
+}
+
+func mockFCMServer() *httptest.Server {
+	router := chi.NewRouter()
+	router.Get("/fcm/send", func(w http.ResponseWriter, r *http.Request) {
+		s := `{"ok": true,
+				"result": {
+					"first_name": "comments_test",
+					"id": 707381019,
+					"is_bot": true,
+					"username": "remark42_test_bot"
+				}}`
+		_, _ = w.Write([]byte(s))
+	})
+	return httptest.NewServer(router)
+}
